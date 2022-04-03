bf249051338f9d8bb5470218486696a5edef09bb
DarccMan <darcy3057@gmail.com>
2022-04-03 11:20:33 +1000

    Fix bugs, Add tones, Clean code

--- script.js
+++ script.js
@@ -24,8 +24,21 @@ const sliders = {
     name: "Blue",
     max: 255,
   },
+
+  c: {
+    name: "Cyan",
+    max: 100,
+  },
+  m: {
+    name: "Magenta",
+    max: 100,
+  },
+  y: {
+    name: "Yellow",
+    max: 100,
+  },
 };
-var hex;
+var lastHsv;
 
 // Initialize
 function init() {
@@ -56,28 +69,98 @@ function change(id) {
   }
 
   // Standardize colors
-  var color;
-  if ("rgb".includes(id)) {
+  if ("hsv".includes(id)) {
+    var rgb = F.hsv2rgb(current);
+
+    $("#r").val(rgb.r);
+    $("#g").val(rgb.g);
+    $("#b").val(rgb.b);
+
+    var cmy = hsv2cmy(current);
+    $("#c").val(cmy.c);
+    $("#y").val(cmy.y);
+    $("#m").val(cmy.m);
+    $("#k").val(cmy.k);
+  } else if ("rgb".includes(id)) {
     var hsv = F.rgb2hsv(current);
-    color = [current.r, current.g, current.b].join(", ");
+    // Preserve HSV when RGB = 0 or 255
+    if (hsv.s && hsv.v) {
+      // lastHsv = hsv;
+    } else if (lastHsv) {
+      // hsv.h = lastHsv.h;
+      if (current.r + current.g + current.b === 0) {
+        // hsv.s = lastHsv.s;
+      }
+    }
+
+    $("#h").val(hsv.h);
+    $("#s").val(hsv.s);
+    $("#v").val(hsv.v);
+
+    var cmy = rgb2cmy(current);
+    $("#c").val(cmy.c);
+    $("#y").val(cmy.y);
+    $("#m").val(cmy.m);
+    $("#k").val(cmy.k);
+  } else if ("cmyk".includes(id)) {
+    var rgb = cmy2rgb(current);
+
+    $("#r").val(rgb.r);
+    $("#g").val(rgb.g);
+    $("#b").val(rgb.b);
+
+    var hsv = F.rgb2hsv(rgb);
+    // Preserve HSV when RGB = 0 or 255
+    if (hsv.s && hsv.v) {
+      // lastHsv = hsv;
+    } else if (lastHsv) {
+      // hsv.h = lastHsv.h;
+      if (rgb.r + rgb.g + rgb.b === 0) {
+        // hsv.s = lastHsv.s;
+      }
+    }
 
     $("#h").val(hsv.h);
     $("#s").val(hsv.s);
     $("#v").val(hsv.v);
   } else {
-    var rgb = F.hsv2rgb(current);
-    color = [rgb.r, rgb.g, rgb.b].join(", ");
-
-    $("#r").val(rgb.r);
-    $("#g").val(rgb.g);
-    $("#b").val(rgb.b);
+    console.error("Invalid ID");
   }
-  $("#display").css("background-color", "rgb(" + color + ")");
 
   // Reset current values
   for (var i in sliders) {
     current[i] = parseInt($("#" + i).val());
     $("#value_" + i).text(current[i]);
+  }
+
+  // Set hex value as text
+  var hex = F.rgb2hex(current, true);
+  $("#hex")
+    .text(hex)
+    .css("color", current.v > 50 && current.s < 50 ? "black" : "white");
+
+  // Set color of display and tones
+  var tones = {
+    color: [0, 0],
+    darker: [10, -20],
+    dark: [5, -10],
+    light: [-5, 10],
+    lighter: [-10, 20],
+  };
+  var multiply = 0.5;
+  var hsv = F.rgb2hsv(current);
+  for (var i in tones) {
+    var value = F.hsv2hex(
+      {
+        ...hsv,
+        s: F.border(hsv.s + tones[i][0] * multiply, 0, 100),
+        v: F.border(hsv.v + tones[i][1] * multiply, 0, 100),
+      },
+      true,
+    );
+    $("#" + i).css("background-color", value);
+    $("#" + i).attr("title", value);
+    $("#" + i).attr("value", value);
   }
 
   // Set slider gradients
@@ -96,6 +179,10 @@ function change(id) {
     r: [F.rgb2hex({ ...current, r: 0 }), F.rgb2hex({ ...current, r: 255 })],
     g: [F.rgb2hex({ ...current, g: 0 }), F.rgb2hex({ ...current, g: 255 })],
     b: [F.rgb2hex({ ...current, b: 0 }), F.rgb2hex({ ...current, b: 255 })],
+
+    c: [cmy2hex({ ...current, c: 0 }), cmy2hex({ ...current, c: 100 })],
+    m: [cmy2hex({ ...current, m: 0 }), cmy2hex({ ...current, m: 100 })],
+    y: [cmy2hex({ ...current, y: 0 }), cmy2hex({ ...current, y: 100 })],
   };
   for (var i in backgrounds) {
     $("#" + i).css(
@@ -103,12 +190,6 @@ function change(id) {
       `linear-gradient(to right, ${backgrounds[i].join(", ")})`,
     );
   }
-
-  // Set hex value
-  hex = F.rgb2hex(current).slice(0, -2);
-  $("#hex")
-    .text(hex)
-    .css("color", current.v > 50 && current.s < 50 ? "black" : "white");
 }
 
 // Set sliders to random color
@@ -120,7 +201,57 @@ function random() {
   change("r");
 }
 
+// Many random colors in a row
+async function roll() {
+  if (!localStorage.shownWarning) {
+    if (!confirm("Flash warning!\nPress OK to continue")) {
+      return;
+    }
+
+    localStorage.shownWarning = true;
+  }
+
+  random();
+  for (var i = 0; i <= 5; i++) {
+    await F.sleep(80);
+    random();
+  }
+}
+
 // Copy hex value to clipboard
-function copy() {
-  F.copy(hex);
+function copyValue(element) {
+  F.copy($(element).attr("value"));
+}
+
+// Convert CMY to RGB
+function cmy2rgb(cmy) {
+  return {
+    r: (100 - cmy.c) * 2.55,
+    g: (100 - cmy.m) * 2.55,
+    b: (100 - cmy.y) * 2.55,
+  };
+}
+
+// Convert RGB to CMY
+function rgb2cmy(rgb) {
+  return {
+    c: (255 - rgb.r) / 2.55,
+    m: (255 - rgb.g) / 2.55,
+    y: (255 - rgb.b) / 2.55,
+  };
+}
+
+// Convert CMY to hex
+function cmy2hex(cmy) {
+  return F.rgb2hex(cmy2rgb(cmy));
+}
+
+// Convert CMY to HSV
+function cmy2hsv(cmy) {
+  return F.rgb2hsv(cmy2rgb(cmy));
+}
+
+// Convert HSV to CMY
+function hsv2cmy(hsv) {
+  return rgb2cmy(F.hsv2rgb(hsv));
 }
